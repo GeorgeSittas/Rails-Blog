96fae7d93318ce7b714d916592aa1ac7d10f40aa
GeorgeSittas <giwrgos.sittas@gmail.com>
2021-03-13 22:34:20 +0200

    Added a route for ArticlesController & displayed message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# For details on the DSL available within this file, see:
+# https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+	# This route below is a *rule* which declares that "GET /articles"
+	# requests are mapped to the "index" action of ArticlesController
+
+	get "/articles", to: "articles#index"
+
 end
