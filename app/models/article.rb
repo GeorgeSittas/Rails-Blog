22ad17b7cddacbabbf7152e3f57c2afae1ae178d
GeorgeSittas <giwrgos.sittas@gmail.com>
2021-03-16 22:23:03 +0200

    Add error messages & link the 'new' article page in index

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+	# 1st validation: declares that title field must be present (at least 1 non-white char)
+	# 2nd validation: declares that body must be present, with length at minimum equal to 10
+
 	validates :title, presence: true
 	validates :body, presence: true, length: { minimum: 10 }
 end
