e74aac257456a6467977d4a071b013ab4f91cacf
GeorgeSittas <giwrgos.sittas@gmail.com>
2021-03-16 21:11:00 +0200

    Add new and create actions -> ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,25 @@ class ArticlesController < ApplicationController
   def show
   	@article = Article.find(params[:id]) # Fetch a single article
   end
+
+  def new
+  	# Instantiates a new article, without saving it. This will be used
+  	# in the view when building the form for article creation.
+
+  	@article = Article.new
+  end
+
+  def create
+  	# Instantiates a new article with values for the title and body, which
+  	# is to be saved into the database.
+
+  	@article = Article.new(title: "...", body: "...")
+
+  	if @article.save
+  		redirect_to @article # redirect_to forces browser to make a new request
+  	else
+  		render :new # Renders the specified view for the current request
+  	end
+  end
+
 end
