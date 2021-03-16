b54020b6ec38f037ad150a5950e44c2784559189
GeorgeSittas <giwrgos.sittas@gmail.com>
2021-03-16 19:52:22 +0200

    Add articles#show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
   	@articles = Article.all # Fetch all articles from the database
   end
+
+  def show
+  	@article = Article.find(params[:id]) # Fetch a single article
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,15 @@ Rails.application.routes.draw do
 
 	get "/articles", to: "articles#index"
 
+	# This time, our root has something new: a route parameter (:id). A route
+	# parameter captures a segment of the reques's path, and puts that value
+	# into the params Hash, which is accessible by the controller action.
+	#
+	# For example, when handling a request like:
+	# GET http://localhost:3000/articles/1,
+	# 1 would be captured as the value for :id, which would then be accessible
+	# as params[:id] in the show action of ArticlesController.
+
+	get "/articles/:id", to: "articles#show"
+
 end
