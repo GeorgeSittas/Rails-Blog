5f0bcf17fda30180c67b0b4383a2a150b4058216
GeorgeSittas <giwrgos.sittas@gmail.com>
2021-03-19 18:57:44 +0200

    Add create action for Comment, add comment displaying in show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+	def create
+		# Slightly more complicated than the ArticlesController, since we have
+		# to track the corresponding article that each comment is attached to.
+
+		# Fetches the article in question
+		@article = Article.find(params[:article_id])
+
+		# Creates and saves a comment. Automatically links it so that it belongs
+		# to the particular article.
+		@comment = @article.comments.create(comment_params)
+
+		redirect_to article_path(@article)
+	end
+
+	private
+		def comment_params
+			params.require(:comment).permit(:commenter, :body)
+		end
 end
