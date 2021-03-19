e6617187110fbe370982c6939fe2a2429bdb0f13
GeorgeSittas <giwrgos.sittas@gmail.com>
2021-03-19 19:43:08 +0200

    Deleting comments associated to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
 	include Visible # Module which provides visibility functionality for an article
 
-	has_many :comments # Declares that an article may be associated to many comments
+	# Declares that an article may be associated to many comments.
+	# dependent: :destroy means that upon article deletion, all its comments
+	# will also be deleted, so that they won't take up space in the database.
+	has_many :comments, dependent: :destroy
 
 	# 1st validation: declares that title field must be present (at least 1 non-white char)
 	# 2nd validation: declares that body must be present, with length at minimum equal to 10
